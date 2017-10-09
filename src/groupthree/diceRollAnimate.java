853c63cef101882bed135f64b538fc408ee67f6d
Wes Harrison <harriswe@mail.gvsu.edu>
2017-10-09 17:48:29 -0400

    Extended controller class to include dice roll class, which includes the animation method.

---
+++ src/groupthree/diceRollAnimate.java
@@ -0,0 +1,13 @@
+package groupthree;
+
+import javafx.animation.Timeline;
+
+public class diceRollAnimate extends FarkleController {
+
+    public void cycleImages() {
+        final Timeline diceImages = new Timeline();
+
+
+    }
+
+}
--- src/groupthree/mainUI.java
+++ src/groupthree/mainUI.java
@@ -7,9 +7,21 @@ import javafx.scene.Scene;
 
 import javafx.stage.Stage;
 
+/**
+ * mainUI is the base class for the Farkle game. This allows for the basic construction of an FXML-assisted GUI
+ * development as afforded by JavaFX Scene Builder 9.0. The class extends javafx.application.Application,
+ * thus the start method is overridden to follow syntax.
+ * @author Wes Harrison
+ * @version 1.0
+ */
 public class mainUI extends Application {
 
-
+    /**
+     *
+     * @param primaryStage Default override parameter.
+     *                     @see javafx.stage.Stage
+     * @throws Exception If and only if the FXML file is not found. In this case, we know the files are implemented in src.
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
 
@@ -26,6 +38,10 @@ public class mainUI extends Application {
 
     }
 
+    /**
+     * The main method for
+     * @param args normal command line argument input
+     */
     public static void main(String[] args) {
         launch(args);
 
