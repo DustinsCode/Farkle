d8b5c23fe17a7f9d2d75b6b173edc3768a59f541
Wes Harrison <harriswe@mail.gvsu.edu>
2017-10-24 14:18:47 -0400

    Merge DiceUITest
    Updated java files too in groupthree.

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"testing","l":"ControllerTest"},{"p":"groupthree","l":"Dice"},{"p":"groupthree","l":"DiceUILogic"},{"p":"testing","l":"DiceUILogicTest"},{"p":"testing","l":"FakeController"},{"p":"groupthree","l":"FarkleController"},{"p":"groupthree","l":"FarkleInterface"},{"p":"groupthree","l":"GameLogic"},{"p":"groupthree","l":"MainUI"}]
